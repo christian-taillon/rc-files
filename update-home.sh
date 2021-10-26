2f0df1696330ab11173dfd6ec8b685d72fccda7c
christian <christianjtaillon@gmail.com>
2021-10-26 08:49:43 -0700

    Added more comments/echos

--- update-home.sh
+++ update-home.sh
@@ -1,6 +1,12 @@
+echo "copying bash_aliases to home dir"
 cp ./.bash_aliases ~/.
+echo "copying bashrc to home dir"
 cp ./.bashrc ~/.
+echo "copying vimrc to home dir"
 cp ./.vimrc ~/.
+echo "sourcing bash_aliases"
 source ~/.bash_aliases
+echo "sourcing bashrc"
 source ~/.bashrc
+echo "complete"
 echo "run ':source ~/.vimrc' inside vim"
